45df7171b52a11a6e91fa67ba5ce26c7282016f6
xevi-adams <shinronster@gmail.com>
2016-05-11 09:14:53 +0200

    Add files via upload

---
+++ templates/footer.php
@@ -0,0 +1,19 @@
+
+
+<footer class="navbar navbar-default navbar-fixed-bottom">
+
+    Copyright 2016
+
+</footer>
+
+</div>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="static/js/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="static/js/bootstrap.min.js"></script>
+
+</body>
+</html>
+
+
---
+++ templates/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Projecte</title>
+
+    <!-- Slider -->
+    <link href="static/css/slider.css" rel="stylesheet" type="text/css">
+
+    <!-- Bootstrap -->
+    <link href="static/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<header>
+
+    <h1 class="text-center">La meva primera pagina en PHP</h1>
+
+    <?php
+    require_once('templates/menu.php');
+    ?>
+
+</header>
+
+<div>
+
+
